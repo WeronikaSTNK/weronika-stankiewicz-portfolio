794a84ee5fb52e83ae80d6f3faef890bc06db1fc
Weronika Stankiewicz <weronika.stankiewicz94@gmail.com>
2020-03-03 13:14:52 +0100

    add skills and npm packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Portfolio from './Layout/Portfolio';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<Portfolio />, document.getElementById('root'));
 

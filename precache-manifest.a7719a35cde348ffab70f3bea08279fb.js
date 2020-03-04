05d4110404b2140ff6f26333a93d931bef8a7483
Weronika Stankiewicz <weronika.stankiewicz94@gmail.com>
2020-03-04 13:43:50 +0100

    Updates

---
+++ precache-manifest.a7719a35cde348ffab70f3bea08279fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1211e0df4e36cd7616aad1714d2ad4",
+    "url": "/weronika-stankiewicz-portfolio/index.html"
+  },
+  {
+    "revision": "2a386b256b390431d18b",
+    "url": "/weronika-stankiewicz-portfolio/static/css/2.fc255550.chunk.css"
+  },
+  {
+    "revision": "36c4f0db1124f728e7b2",
+    "url": "/weronika-stankiewicz-portfolio/static/css/main.5646a1a5.chunk.css"
+  },
+  {
+    "revision": "2a386b256b390431d18b",
+    "url": "/weronika-stankiewicz-portfolio/static/js/2.5e8cf67c.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/weronika-stankiewicz-portfolio/static/js/2.5e8cf67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c4f0db1124f728e7b2",
+    "url": "/weronika-stankiewicz-portfolio/static/js/main.413b6d35.chunk.js"
+  },
+  {
+    "revision": "8065549a43e2a6e16c88",
+    "url": "/weronika-stankiewicz-portfolio/static/js/runtime-main.e6302750.js"
+  },
+  {
+    "revision": "1dd42986ec6d3396d9e9c8ee2bad315f",
+    "url": "/weronika-stankiewicz-portfolio/static/media/avatar.1dd42986.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weronika-stankiewicz-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weronika-stankiewicz-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weronika-stankiewicz-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weronika-stankiewicz-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weronika-stankiewicz-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e20a2fde4dce51b33373fbfde1eb5e46",
+    "url": "/weronika-stankiewicz-portfolio/static/media/polandrock.e20a2fde.png"
+  }
+]);
